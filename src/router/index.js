77519207b44391b1205ee664b3c11c332d4ad88b
623343812 <623343812@qq.com>
2020-12-31 14:57:42 +0800

    完成订单功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import GoodList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
 
 Vue.use(VueRouter)
 
@@ -59,6 +60,10 @@ const routes = [
     {
       path: '/goods/add',
       component:Add
+    },
+    {
+      path: '/orders',
+      component:Order
     },
   ]
   },
